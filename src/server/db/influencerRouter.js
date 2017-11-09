bb6dfab094fb9c349277d58dcb81973531fe3b80
npatrick <wazzupnickz@gmail.com>
2017-11-09 15:19:54 -0800

    add schema routers

---
+++ src/server/db/influencerRouter.js
@@ -0,0 +1,21 @@
+const influencerRouter = require('express').Router();
+const influencerController = require('./influencerController.js');
+
+// Route handlers for each of the controllers
+
+influencerRouter.route('/').get((req, res) => res.send('Influencers Page!'));
+
+influencerRouter.route('api/influencer').get((req, res) => {
+  var temp = influencerController.retrieve(req, res);
+  res.send(temp);
+});
+
+influencerRouter.route('api/influencer').post((req, res) => influencerController.createOne(req, res));
+
+influencerRouter.route('api/influencer/:username').get((req, res) => influencerController.retrieveOne(req, res));
+
+influencerRouter.route('api/influencer/:username').put((req, res) => influencerController.updateOne(req, res));
+
+influencerRouter.route('api/influencer/:username').delete((req, res) => influencerController.deleteOne(req, res));
+
+influencerRouter.route('api/influencer').delete((req, res) => influencerController.delete(req, res));
