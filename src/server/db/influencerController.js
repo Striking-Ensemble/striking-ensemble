9de3e999b07e56819c793dca0872172150cdf340
npatrick <wazzupnickz@gmail.com>
2017-11-09 15:08:08 -0800

    add schema controller

---
+++ src/server/db/influencerController.js
@@ -0,0 +1,60 @@
+const Influencer = require('./Influencer.js');
+const bodyParser = require('body-parser');
+const app = require('../server.js');
+
+// Controller methods
+
+exports.createOne = (req, res) => {
+  Influencer.create(req.body.data, (err, data) => {
+    console.log('ON CREATEONE, ADDING:', req.body.data);
+    if (err) {
+      throw err;
+    }
+    res.send('We created one!');
+  });
+};
+
+exports.retrieve = (req, res) => {
+  Influencer.find((err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send(data);
+  });
+};
+
+exports.retrieveOne = (req, res) => {
+  Influencer.find(req.body.data.username, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send(data);
+  });
+};
+
+exports.updateOne = (req, res) => {
+  Influencer.update(req.body.data.username, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send('Influencer is updated:', data);
+  });
+};
+
+exports.delete = (req, res) => {
+  Influencer.remove((err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send('DELETED!:', data);
+  });
+};
+
+exports.deleteOne = (req, res) => {
+  Influencer.remove(req.body.data.number, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send('Deleted!:', data);
+  })
+};
