f1801bc80fa0aff7dda447172b881f20249cc5b2
npatrick <wazzupnickz@gmail.com>
2017-11-10 15:13:42 -0800

    fix api path

--- src/server/db/influencerRouter.js
+++ src/server/db/influencerRouter.js
@@ -1,44 +1,45 @@
-const influencerRouter = require('express').Router();
+const router = require('express').Router();
 const influencerController = require('./influencerController.js');
+const bodyParser = require('body-parser');
 
 // middleware that is specific to this router
-influencerRouter.use(function timeLog (req, res, next) {
+router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
 
 // Route handlers for each of the controllers
 
-influencerRouter.get('/', (req, res) => res.send('Influencers Page!'));
+router.get('/', (req, res) => res.send('Influencers Page!'));
 
-influencerRouter.get('/api/influencer', (req, res) => {
+router.get('/influencer', (req, res) => {
   console.log('CHECKING GET INFLUENCER');
   influencerController.retrieve(req, res);
 });
 
-influencerRouter.post('/api/influencer', (req, res) => {
+router.post('/influencer', (req, res) => {
   console.log('CHECKING POST ADDING INFLUENCER');
   influencerController.createOne(req, res)
 });
 
-influencerRouter.get('/api/influencer/:username', (req, res) => { 
+router.get('/influencer/:username', (req, res) => { 
   console.log('CHECKING GET ONE INFLUENCER');
   influencerController.retrieveOne(req, res)
 });
 
-influencerRouter.put('/api/influencer/:username', (req, res) => { 
+router.put('/influencer/:username', (req, res) => { 
   console.log('CHECKING PUT, UPDATE ONE INFLUENCER');
   influencerController.updateOne(req, res)
 });
 
-influencerRouter.delete('/api/influencer/:username', (req, res) => { 
+router.delete('/influencer/:username', (req, res) => { 
   console.log('CHECKING DELETE ONE INFLUENCER');
   influencerController.deleteOne(req, res)
 });
 
-influencerRouter.delete('/api/influencer', (req, res) => { 
+router.delete('/influencer', (req, res) => { 
   console.log('CHECKING DELETE INFLUENCER COLLECTION');
   influencerController.delete(req, res)
 });
 
-module.exports = influencerRouter;
+module.exports = router;
