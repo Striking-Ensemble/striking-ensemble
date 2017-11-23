c206f3c2153e8dca178f4088516f5fb6f0f3b13f
npatrick <wazzupnickz@gmail.com>
2017-11-22 19:02:48 -0800

    fix resolving file extensions

--- src/client/components/app.react.jsx
+++ src/client/components/app.react.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-// import Signin from '../scenes/Sign/signin.react.jsx';
-import Navigation from './navigation.react.jsx';
-import Account from '../scenes/Home/account.react.jsx';
-import Footer from './footer.react.jsx';
+// import Signin from '../scenes/Sign/signin.react';
+import Navigation from './navigation.react';
+import Account from '../scenes/Home/account.react';
+import Footer from './footer.react';
 
 export default class App extends Component {
   constructor (props) {
