51a8fe0175b29779f95e7289ddb722169ec10d64
npatrick <wazzupnickz@gmail.com>
2017-11-27 17:08:47 -0800

    remove comments

--- src/client/scenes/Sign/signin.react.jsx
+++ src/client/scenes/Sign/signin.react.jsx
@@ -3,39 +3,6 @@ import axios from 'axios';
 import store from 'store'
 
 export default class Signin extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     error: null,
-  //     isLoaded: false,
-  //     user: [],
-  //   }
-  // }
-
-  // componentDidMount() {
-  //   axios.get(ROOT_URL + '/account')
-  //     .then(
-  //     (res) => {
-  //       this.setState({
-  //         isLoaded: true,
-  //         user: res
-  //       });
-  //       const { history } = this.props;
-  //       store.set('user', { username: res.username })
-  //       console.log('IN SIGNIN, RES?', res);
-  //       store.set('isAuthenticated', res.isAuthenticated);
-  //       history.push('/account');
-  //     },
-  //     (err) => {
-  //       this.setState({
-  //         isLoaded: true,
-  //         error: err
-  //       });
-  //     }
-  //     );
-  // }
-
   render() {
     return (
       <div className="container">
