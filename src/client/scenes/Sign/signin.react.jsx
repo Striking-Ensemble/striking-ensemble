dd472a350ef064c1a4795351975ab722eece68a4
npatrick <wazzupnickz@gmail.com>
2017-12-04 20:35:58 -0800

    fix comments and component structure

--- src/client/scenes/Sign/signin.react.jsx
+++ src/client/scenes/Sign/signin.react.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import store from 'store'
+import Footer from '../../components/footer.react';
 
 export default class Signin extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="signin">
-        <h1>Welcome!</h1>
-        <a href="/auth/instagram" className="btn btn-block btn-social btn-instagram">
-          <span className="fa fa-instagram"></span> Sign in with Instagram
-        </a>
+      <div id="page-outer">
+        <div className="container">
+          <div className="signin">
+          <h1>Welcome!</h1>
+          <a href="/auth/instagram" className="btn btn-block btn-social btn-instagram">
+            <span className="fa fa-instagram"></span> Sign in with Instagram
+          </a>
+          </div>
         </div>
+        <Footer />
       </div>
     )
   }
