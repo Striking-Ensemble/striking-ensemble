6286c089340af4ac1920173fd6c11cf3d7602c68
npatrick <wazzupnickz@gmail.com>
2017-12-14 16:52:38 -0800

    refactor media schema into a collection

--- src/server/db/media.js
+++ src/server/db/media.js
@@ -7,59 +7,57 @@ mongoose.Promise = global.Promise;
 const db = require('./index');
 
 const mediaSchema = Schema({
+  _id: String,
   _creator: {
     type: Number,
     ref: 'Influencer'
   },
-  data: { type: [{
-    instaId: String,
-    caption: { type: [{
-      id: String,
-      text: String,
-      created_time: String
-    }]},
-    created_time: String,
-    images: { type: [{
-      low_resolution: { type: [{
-        width: Number,
-        height: Number,
-        url: String
-      }]},
-      standard_resolution: { type: [{
-        width: Number,
-        height: Number,
-        url: String
-      }]},
-      thumbnail: { type: [{
-        width: Number,
-        height: Number,
-        url: String
-      }]}
-    }]},
-    link: String,
-    tags: Array,
-    post_type: String,
-    videos: { type: [{
-      low_bandwidth: { type: [{
-        width: Number,
-        height: Number,
-        url: String
-      }]},
-      low_resolution: { type: [{
-        width: Number,
-        height: Number,
-        url: String
-      }]},
-      standard_resolution: { type: [{
-        width: Number,
-        height: Number,
-        url: String
-      }]}
-    }]},
-    retailLinks: { type: [{ 
-      index: String, 
+  caption: { type: [{
+    id: String,
+    text: String,
+    created_time: String
+  }]},
+  created_time: String,
+  images: { type: [{
+    low_resolution: { type: [{
+      width: Number,
+      height: Number,
       url: String
-    }]} 
+    }]},
+    standard_resolution: { type: [{
+      width: Number,
+      height: Number,
+      url: String
+    }]},
+    thumbnail: { type: [{
+      width: Number,
+      height: Number,
+      url: String
+    }]}
+  }]},
+  link: String,
+  tags: Array,
+  post_type: String,
+  videos: { type: [{
+    low_bandwidth: { type: [{
+      width: Number,
+      height: Number,
+      url: String
+    }]},
+    low_resolution: { type: [{
+      width: Number,
+      height: Number,
+      url: String
+    }]},
+    standard_resolution: { type: [{
+      width: Number,
+      height: Number,
+      url: String
+    }]}
+  }]},
+  retailLinks: { type: [{ 
+    index: String, 
+    url: String
   }]}
 });
 
