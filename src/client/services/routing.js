f5074987d539b05429d949d0aba829da0052cf7a
npatrick <wazzupnickz@gmail.com>
2017-12-30 01:11:02 -0800

    add 404 component

--- src/client/services/routing.js
+++ src/client/services/routing.js
@@ -7,42 +7,18 @@ import PostItem from '../scenes/Home/postItem.react';
 import Navigation from '../components/navigation.react';
 import Footer from '../components/footer.react';
 import PostListItem from '../scenes/Home/postListItem.react';
-import NotFound from '../components/notFound.react';
+import FourOhFour from '../components/fourOhFour.react';
 import Consumer from '../scenes/consumer.react';
-
-// const routes = [
-//   { path:'/login',
-//     mainContent: () => <Signin />
-//   },
-//   { path: '/account/post/:id',
-//     exact: true,
-//     nav: () => <Navigation />,
-//     mainContent: () => <PostItem />,
-//     footer: () => <Footer />
-//   },
-//   {
-//     path: '/',
-//     nav: () => <Navigation />,
-//     mainContent: () => <Influencer />,
-//     footer: () => <Footer />
-//   },
-//   {
-//     path: '/account',
-//     nav: () => <Navigation />,
-//     mainContent: () => <Influencer />,
-//     footer: () => <Footer />
-//   }
-// ];
 
 const Routing = () => {
   return (
     <Switch>
       <Route exact path="/login" component={Landing} />
-      <Route path="/account" component={Influencer} />
+      <Route exact path="/account" component={Influencer} />
       <Route exact path="/account/post/:id" component={Account} />
-      <Route path="/:username" component={Consumer} />
-      <Route exact path="/404" component={NotFound} />
+      <Route exact path="/:username" component={Consumer} />
       <Route exact path="/" component={Influencer} />
+      <Route component={FourOhFour} />
     </Switch>
   )
 }
