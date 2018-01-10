31da86cd2329368f1e54f7a2ef75061b04849418
npatrick <wazzupnickz@gmail.com>
2018-01-09 16:35:15 -0800

    add stripe config and keys

--- src/server/config/keys.js
+++ src/server/config/keys.js
@@ -1,9 +1,23 @@
-const callbackAddress = (process.env.HOST? `${process.env.HOST}/auth/instagram/callback` : undefined) || 'http://localhost:3000/auth/instagram/callback';
-const INSTAGRAM_CLIENT_ID = process.env.Instagram_client_id;
-const INSTAGRAM_CLIENT_SECRET = process.env.Instagram_client_secret;
+'use strict';
 
 module.exports = {
-  clientID: INSTAGRAM_CLIENT_ID,
-  clientSecret: INSTAGRAM_CLIENT_SECRET,
-  callbackURL: callbackAddress,
+  secret: 'sessionSecret',
+  // Configuration for Instagram Passport
+  clientID: process.env.Instagram_client_id,
+  clientSecret: process.env.Instagram_client_secret,
+  callbackURL: (process.env.HOST ? `${process.env.HOST}/auth/instagram/callback` : undefined) || 'http://localhost:3000/auth/instagram/callback',
+  // Configuration for Stripe.
+  // API Keys: https://dashboard.stripe.com/account/apikeys
+  // Connect Settings: https://dashboard.stripe.com/account/applications/settings
+  stripe: {
+    secretKey: process.env.Stripe_secretKey,
+    publishableKey: process.env.Stripe_publishableKey,
+    clientId: 'YOUR_STRIPE_CLIENT_ID',
+    authorizeUri: 'https://connect.stripe.com/express/oauth/authorize',
+    tokenUri: 'https://connect.stripe.com/oauth/token'
+  },
+  twoTap: {
+    publicToken: process.env.TwoTap_public_token,
+    privateToken: process.env.TwoTap_private_token
+  }
 };
