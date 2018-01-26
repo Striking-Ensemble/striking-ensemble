7aad64eed81f8422dcd061ba23dc20f7958dfc8a
npatrick <wazzupnickz@gmail.com>
2018-01-25 23:28:12 -0800

    add contact info form

--- src/client/scenes/Profile-Settings/profileSettings.react.jsx
+++ src/client/scenes/Profile-Settings/profileSettings.react.jsx
@@ -90,7 +90,7 @@ export default class ProfileSettings extends Component {
               <div className="form-group">
                 <label className="col-md-3 col-sm-3 col-xs-8" htmlFor="city">City</label>
                 <div className="col-md-9 col-sm-9">
-                  <input type="email" className="form-control" id="cityInput" placeholder="Bakedfield" />
+                  <input type="text" className="form-control" id="cityInput" placeholder="Bakedfield" />
                 </div>
               </div>
               <div className="form-group">
@@ -108,8 +108,65 @@ export default class ProfileSettings extends Component {
             </div>
           </div>
         </div>
+        <div className="box-it">
+          <h3>Contact Info</h3>
+          <div className="row">
+            <div className="col-lg-5 col-lg-offset-1 col-md-6 col-sm-6">
+              <div className="form-group">
+                <label className="col-md-3 col-sm-3 col-xs-8" htmlFor="firstName">First Name</label>
+                <div className="col-md-9 col-sm-9">
+                  <input type="text" className="form-control" id="firstNameInput" placeholder="Jane" required />
+                </div>
+              </div>
+              <div className="form-group">
+                <label className="col-md-3 col-sm-3 col-xs-8" htmlFor="lastName">Last Name</label>
+                <div className="col-md-9 col-sm-9">
+                  <input type="text" className="form-control" id="lastNameInput" placeholder="Doe" />
+                </div>
+              </div>
+              <div className="form-group">
+                <label className="col-md-3 col-sm-3 col-xs-8" htmlFor="email">Email</label>
+                <div className="col-md-9 col-sm-9">
+                  <input type="email" className="form-control" id="emailInput" placeholder="janedoe@email.com" />
+                </div>
+              </div>
+              <div className="form-group">
+                <label className="col-md-3 col-sm-3 col-xs-8" htmlFor="securityQuestion">Security Question</label>
+                <div className="col-md-9 col-sm-9">
+                  <select className="form-control">
+                    <option>Please Select</option>
+                    <option>What is your pet's name?</option>
+                    <option>What was the name of your first school?</option>
+                    <option>Who was your childhood hero?</option>
+                    <option>What is your favorite pastime?</option>
+                    <option>What is your all-time favorite sports team?</option>
+                    <option>What is your father's middle name?</option>
+                    <option>What was your high school mascot?</option>
+                    <option>What make was your first car?</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-group">
+                <label className="col-md-3 col-sm-3 col-xs-8" htmlFor="phone">Answer</label>
+                <div className="col-md-9 col-sm-9">
+                  <input type="text" className="form-control" id="answerInput" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" /><span> I have read and accept the terms of the Influencer Membership Agreement</span>
+            </label>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" /><span> I am 18 years of age or older</span>
+            </label>
+          </div>
+        </div>
         <div className="row">
-          <button type="submit" className="btn btn-success pull-right">Save</button>
+          <button type="submit" className="btn btn-success center-block">Save</button>
         </div>
       </form>
     )
