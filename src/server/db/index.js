2ef9a3218727bc9f75057ea6dc02634fdb0c571e
npatrick <wazzupnickz@gmail.com>
2018-01-31 18:33:26 -0800

    fill user and pass with env

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/influencers'
+const mongoUri = `mongodb://${process.env.DBUser}:${process.env.DBPass}@ds249707.mlab.com:49707/striking-ensemble-sandbox-db/influencers`
 
 const dbOptions = {
   useMongoClient: true,
