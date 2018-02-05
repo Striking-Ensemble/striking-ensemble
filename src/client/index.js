94bf3800e499d507bb4139297b11e4d350064729
npatrick <wazzupnickz@gmail.com>
2018-02-05 14:12:56 -0800

    add conditional build of index in client

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import Routing from './services/routing';
 import store from 'store';
 
 const protocol = window.location.protocol,
-			host = window.location.host,
-			pathname = window.location.pathname;
+	host = window.location.host,
+	pathname = window.location.pathname;
 
 store.set('URL', {
 	protocol: protocol,
@@ -15,20 +14,27 @@ store.set('URL', {
 	root_url: `${protocol}//${host}`
 });
 
-const renderFn = Component => {
+if (process.env.NODE_ENV === 'production') {
 	render((
-		<AppContainer>
-			<Component />
-		</AppContainer>
-	), document.getElementById('App'));
-};
+		<Routing />
+	), document.getElementById('App'))
+} else {
+	const { AppContainer } = require('react-hot-loader');
+	const renderFn = Component => {
+		render((
+			<AppContainer>
+				<Component />
+			</AppContainer>
+		), document.getElementById('App'));
+	};
 
-renderFn(Routing);
+	renderFn(Routing);
 
-// Webpack Hot Module Replacement API
-if (module.hot) {
-	module.hot.accept('./services/routing', () => {
-		const NextApp = require('./services/routing').default;
-		renderFn(NextApp);
-	})
-};
+	// Webpack Hot Module Replacement API
+	if (module.hot) {
+		module.hot.accept('./services/routing', () => {
+			const NextApp = require('./services/routing').default;
+			renderFn(NextApp);
+		})
+	};
+}
