ae32133100cc84e699f4fb44c01e544c33741164
npatrick <wazzupnickz@gmail.com>
2018-02-05 15:28:32 -0800

    fix tabs to spaces for git readability

--- src/client/index.js
+++ src/client/index.js
@@ -4,37 +4,37 @@ import Routing from './services/routing';
 import store from 'store';
 
 const protocol = window.location.protocol,
-	host = window.location.host,
-	pathname = window.location.pathname;
+  host = window.location.host,
+  pathname = window.location.pathname;
 
 store.set('URL', {
-	protocol: protocol,
-	host: host,
-	pathname: pathname,
-	root_url: `${protocol}//${host}`
+  protocol: protocol,
+  host: host,
+  pathname: pathname,
+  root_url: `${protocol}//${host}`
 });
 
 if (process.env.NODE_ENV === 'production') {
-	render((
-		<Routing />
-	), document.getElementById('App'))
+  render((
+    <Routing />
+  ), document.getElementById('App'))
 } else {
-	const { AppContainer } = require('react-hot-loader');
-	const renderFn = Component => {
-		render((
-			<AppContainer>
-				<Component />
-			</AppContainer>
-		), document.getElementById('App'));
-	};
+  const { AppContainer } = require('react-hot-loader');
+  const renderFn = Component => {
+    render((
+      <AppContainer>
+        <Component />
+      </AppContainer>
+    ), document.getElementById('App'));
+  };
 
-	renderFn(Routing);
+  renderFn(Routing);
 
-	// Webpack Hot Module Replacement API
-	if (module.hot) {
-		module.hot.accept('./services/routing', () => {
-			const NextApp = require('./services/routing').default;
-			renderFn(NextApp);
-		})
-	};
+  // Webpack Hot Module Replacement API
+  if (module.hot) {
+    module.hot.accept('./services/routing', () => {
+      const NextApp = require('./services/routing').default;
+      renderFn(NextApp);
+    })
+  };
 };
