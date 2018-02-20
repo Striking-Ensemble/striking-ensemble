abbb20290b9a66f676c3bb3be8f9e9014e6ac2d5
npatrick <wazzupnickz@gmail.com>
2018-02-19 16:19:46 -0800

    test analytics on FE only

--- src/server/routes/integrations.js
+++ src/server/routes/integrations.js
@@ -18,6 +18,9 @@ exports.integration = (req, res) => {
 
 exports.purchaseConfirmCallback = (req, res) => {
   console.log('THIS GOT USED, purchase confirm', req.body);
+  req.body.sites.forEach((storeId) => {
+    storeId
+  })
   /*
   THIS GOT USED, purchase confirm {
     purchase_id: '5a792b00eeba220316d9a275',
@@ -53,35 +56,36 @@ exports.purchaseConfirmCallback = (req, res) => {
     message: 'done'
   }
   */
-  let apiURL = req.app.settings.twoTap_apiUrl;
-  let testMode = req.body.test_mode || 'fake_confirm';
-  let purchaseId = req.body.purchase_id;
-  let uniqueToken = req.body.unique_token;
-  let sites = req.body.sites;
-  const privateToken = req.app.settings.twoTap_private_token;
+  // let apiURL = req.app.settings.twoTap_apiUrl;
+  // let testMode = req.body.test_mode || 'fake_confirm';
+  // let purchaseId = req.body.purchase_id;
+  // let uniqueToken = req.body.unique_token;
+  // let sites = req.body.sites;
+  // const privateToken = req.app.settings.twoTap_private_token;
 
-  let callPath = '/v1.0/purchase/confirm?private_token=' + privateToken;
-  request.post(apiURL + callPath, {
-    form: {
-      purchase_id: purchaseId,
-      test_mode: testMode
-    },
-    json: true
-  }, (err, response, body) => {
-    if (err) {
-      console.log('ERROR Detected on purchaseConfirm', err);
-    }
-    /*
-     * Users should have received a confirmed SMS message
-     * and expected body format from this call is...
-     * {
-     *   "purchase_id": "50f414b9e6a4869bf6000010",
-     *   "message": "still_processing",
-     *   "description": "Still processing."
-     * }
-    */
-    res.send(body);
-  });
+  // let callPath = '/v1.0/purchase/confirm?private_token=' + privateToken;
+  // request.post(apiURL + callPath, {
+  //   form: {
+  //     purchase_id: purchaseId,
+  //     test_mode: testMode
+  //   },
+  //   json: true
+  // }, (err, response, body) => {
+  //   if (err) {
+  //     console.log('ERROR Detected on purchaseConfirm', err);
+  //   }
+  //   /*
+  //    * Users should have received a confirmed SMS message
+  //    * and expected body format from this call is...
+  //    * {
+  //    *   "purchase_id": "50f414b9e6a4869bf6000010",
+  //    *   "message": "still_processing",
+  //    *   "description": "Still processing."
+  //    * }
+  //   */
+  //   res.send(body);
+  // });
+  res.send('FE only testing!');
 };
 
 exports.purchaseUpdatedCallback = (req, res) => {
