4d3ed4d3e15e0dc0f8c06287d041398201d7fa21
npatrick <wazzupnickz@gmail.com>
2018-02-25 12:21:42 -0800

    add reports scene

---
+++ src/client/scenes/Reports/reports.react.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Reports extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Reports Overview</h1>
+  }
+};
