acf1b79699bd54555afa4c98ba7c73469b2f7e0d
npatrick <wazzupnickz@gmail.com>
2018-03-06 19:30:15 -0800

    fix removal of non existing key module

--- src/server/config/googleAuth.js
+++ src/server/config/googleAuth.js
@@ -1,6 +1,5 @@
 const google = require('googleapis').google;
 const googleService = require('./keys').googleService;
-const key = require('../../../Striking-Ensemble-bf76456e2e17');
 
 exports.jwtClient = new google.auth.JWT(
   googleService.client_email, 
