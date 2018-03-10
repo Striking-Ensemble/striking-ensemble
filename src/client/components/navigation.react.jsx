75bafa8645810de1477e8c6ce90d064d6a10903d
Nicko Romana <wazzupnickz@gmail.com>
2018-03-10 14:56:11 -0800

    fix undefined props

--- src/client/components/navigation.react.jsx
+++ src/client/components/navigation.react.jsx
@@ -1,52 +1,43 @@
-import axios from 'axios';
-import store from 'store';
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class Navigation extends Component {
-  constructor(props) {
-    super(props);
+const Navigation = (props) => (
+  // <li> classnames here helps to identify which tab should be highlighted
+  // depending on the current location => props.location
+  <nav className="navbar navbar-inverse navbar-fixed-top">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-header-content" aria-expanded="false">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+        <Link className="navbar-brand" to="/">Striking Ensemble</Link>
+      </div>
+      <div className="collapse navbar-collapse" id="navbar-header-content">
+        <ul className="nav navbar-nav">
+          <li role="button" className={props.location.pathname === '/' || props.location.pathname.includes('account') ? 'active' : ''}>
+            <Link to="/account" data-toggle="collapse" data-target=".navbar-collapse.in">Home <span className="sr-only">(current)</span></Link>
+          </li>
+          <li role="button" className={props.location.pathname.includes('/reports') ? 'active' : ''}>
+            <Link to="/reports" data-toggle="collapse" data-target=".navbar-collapse.in">Reports</Link>
+          </li>
+          <li role="button" className={props.location.pathname.includes('/billing') ? 'active' : ''}>
+            <Link to="/billing" data-toggle="collapse" data-target=".navbar-collapse.in">Billing Info</Link>
+          </li>
+          <li role="button" className={props.location.pathname.includes('/stats') ? 'active' : ''}>
+            <Link to="/settings" data-toggle="collapse" data-target=".navbar-collapse.in">Settings</Link>
+          </li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <Link to="/logout" id="logout-button" className="btn btn-danger log" data-toggle="collapse" data-target=".navbar-collapse.in">Logout</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
 
-  }
-
-  render() {
-    // <li> classnames here helps to identify which tab should be highlighted
-    // depending on the current location => this.props.location
-    return (
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-header-content" aria-expanded="false">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <Link className="navbar-brand" to='/'>Striking Ensemble</Link>
-          </div>
-          <div className="collapse navbar-collapse" id="navbar-header-content">
-            <ul className="nav navbar-nav">
-              <li role="button" className={this.props.location.pathname == '/' || this.props.location.pathname.includes('account') ? "active" : ""}>
-                <Link to='/account' data-toggle="collapse" data-target=".navbar-collapse.in">Home <span className="sr-only">(current)</span></Link>
-              </li>
-              <li role="button" className={this.props.location.pathname.includes('/reports') ? "active" : ""}>
-                <Link to='/reports' data-toggle="collapse" data-target=".navbar-collapse.in">Reports</Link>
-              </li>
-              <li role="button" className={this.props.location.pathname.includes('/billing') ? "active" : ""}>
-                <Link to="/billing" data-toggle="collapse" data-target=".navbar-collapse.in">Billing Info</Link>
-              </li>
-              <li role="button" className={this.props.location.pathname.includes('/stats') ? "active" : ""}>
-                <Link to="/settings" data-toggle="collapse" data-target=".navbar-collapse.in">Settings</Link>
-              </li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link to="/logout" id="logout-button" className="btn btn-danger log" data-toggle="collapse" data-target=".navbar-collapse.in">Logout</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    )
-  }
-};
+export default Navigation;
