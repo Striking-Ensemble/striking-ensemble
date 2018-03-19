94e39b8b101fd15df25f6324a86c3ed758f036a1
npatrick <wazzupnickz@gmail.com>
2018-03-19 16:53:37 -0700

    remove refs on functional components

--- src/client/components/tables.react.jsx
+++ src/client/components/tables.react.jsx
@@ -2,17 +2,10 @@ import React from 'react';
 
 export function Table(props) {
   const { className, style, children } = props; // eslint-disable-line
-  let self = this;
   return (
     <table
       className={className}
       style={style}
-      ref={(node) => {
-          console.log('THIS ON TABLE', self);
-          self.tableNode = node;
-          return self.tableNode;
-        }
-      }
     >
       {children}
     </table>
@@ -21,17 +14,10 @@ export function Table(props) {
 
 export function TableRow(props) {
   const { className, style, children } = props; // eslint-disable-line
-  let self = this;
   return (
     <tr
       className={className}
       style={style}
-      ref={(node) => {
-          console.log('inside row node this is:', self);
-          self.tableRowNode = node;
-          return self.tableRowNode;
-        }
-      }
     >
       {children}
     </tr>
