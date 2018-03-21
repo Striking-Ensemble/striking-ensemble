c4fe67c12b8483433ea3051541dc5d53d7b7cd6a
npatrick <wazzupnickz@gmail.com>
2018-03-21 01:04:33 -0700

    add payout history table

--- src/client/scenes/Billing/payoutList.react.jsx
+++ src/client/scenes/Billing/payoutList.react.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Tables, TableRow } from '../../components/tables.react';
 
 const PayoutList = (props) => {
-  if (props.payoutList.length == 0) {
+  const { payoutList } = props;
+  if (payoutList.length == 0) {
     return <h3 className="col-md-6 col-md-offset-1 col-sm-12">No Payout History Available</h3>
   } else {
     return (
-      <p>{props.payoutList}</p>
+      <Table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Transaction ID</th>
+            <th>Created</th>
+            <th>Arrival Date</th>
+            <th>Amount</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody className="table-striped">
+          {payoutList.map(record => {
+            return (
+              <TableRow key={record.id}>
+                <td>{record.balance_transaction}</td>
+                <td>{record.created}</td>
+                <td>{record.arrival_date}</td>
+                <td>{record.amount}</td>
+                <td>{record.status}</td>
+              </TableRow>
+            ) 
+          })}
+        </tbody>
+      </Table>
     )
   }
 };
