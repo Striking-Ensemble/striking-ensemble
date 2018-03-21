6e6bb2a49985e20e6b801255412db7fb93010207
npatrick <wazzupnickz@gmail.com>
2018-03-21 01:09:14 -0700

    move /login path to down switch logic

--- src/client/services/routing.js
+++ src/client/services/routing.js
@@ -18,12 +18,12 @@ const Routing = () => {
       <div>
         <Route path="/" component={Analytics} />
         <Switch>
-          <Route exact path="/login" component={Landing} />
           <PrivateRoute exact path="/home" component={Influencer} />
           <PrivateRoute exact path="/home/p/:id" component={Influencer} />
           <PrivateRoute path="/billing" component={Influencer} />
           <PrivateRoute path="/settings" component={Influencer} />
           <PrivateRoute path="/reports" component={Influencer} />
+          <Route exact path="/login" component={Landing} />
           <Route exact path="/influencer-list" component={InfluencerList} />
           <Route exact path="/logout" component={Landing} />
           <Route exact path="/:username" component={Consumer} />
